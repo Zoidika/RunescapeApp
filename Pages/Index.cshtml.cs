61074c25b5475906d354d6b80037a62efbb101c5
Zoidika <jacobabrown1994@gmail.com>
2024-11-29 11:31:02 +0000

    Improved error handling

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -35,55 +35,61 @@ namespace RunescapeApp.Pages
         {
             await GetData();
                 Console.WriteLine("Post");
-                
-            try
+            if (!Equipment.All(e => SelectedIds.Contains(e.EquipmentId)))
             {
-                Stats = new Stats();
-                foreach (var item in SelectedIds)
+                ErrorMessage = "Error";
+
+            }
+            else
+            {
+                try
                 {
-                    var newStab = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Stab;
-                    Stats.Stab = Stats.Stab + newStab;
-                    var newSlash = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Slash;
-                    Stats.Slash = Stats.Slash + newSlash;
-                    var newCrush = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Crush;
-                    Stats.Crush = Stats.Crush + newCrush;
-                    var newMagic = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Magic;
-                    Stats.Magic = Stats.Magic + newMagic;
-                    var newRanged = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Ranged;
-                    Stats.Ranged = Stats.Ranged + newRanged;
-                    var newStabDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().StabDef;
-                    Stats.StabDef = Stats.StabDef + newStabDef;
-                    var newSlashDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().SlashDef;
-                    Stats.SlashDef = Stats.SlashDef + newSlashDef;
-                    var newCrushDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().CrushDef;
-                    Stats.CrushDef = Stats.CrushDef + newCrushDef;
-                    var newMagicDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().MagicDef;
-                    Stats.MagicDef = Stats.MagicDef + newMagicDef;
-                    var newRangedDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().RangedDef;
-                    Stats.RangedDef = Stats.RangedDef + newRangedDef;
-                    var newStrBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().StrengthBonus;
-                    Stats.StrengthBonus = Stats.StrengthBonus + newStrBonus;
-                    var newRangedBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().RangedBonus;
-                    Stats.RangedBonus = Stats.RangedBonus + newRangedBonus;
-                    var newMagicBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().MagicBonus;
-                    Stats.MagicBonus = Stats.MagicBonus + newMagicBonus;
-                    var newPrayerBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().PrayerBonus;
-                    Stats.PrayerBonus = Stats.PrayerBonus + newPrayerBonus;
-                    var HasSpecial = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().SpecialAttack;
-                    if (HasSpecial)
+                    Stats = new Stats();
+                    foreach (var item in SelectedIds)
                     {
-                        Stats.SpecialAttack = true;
+                        var newStab = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Stab;
+                        Stats.Stab = Stats.Stab + newStab;
+                        var newSlash = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Slash;
+                        Stats.Slash = Stats.Slash + newSlash;
+                        var newCrush = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Crush;
+                        Stats.Crush = Stats.Crush + newCrush;
+                        var newMagic = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Magic;
+                        Stats.Magic = Stats.Magic + newMagic;
+                        var newRanged = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().Ranged;
+                        Stats.Ranged = Stats.Ranged + newRanged;
+                        var newStabDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().StabDef;
+                        Stats.StabDef = Stats.StabDef + newStabDef;
+                        var newSlashDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().SlashDef;
+                        Stats.SlashDef = Stats.SlashDef + newSlashDef;
+                        var newCrushDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().CrushDef;
+                        Stats.CrushDef = Stats.CrushDef + newCrushDef;
+                        var newMagicDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().MagicDef;
+                        Stats.MagicDef = Stats.MagicDef + newMagicDef;
+                        var newRangedDef = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().RangedDef;
+                        Stats.RangedDef = Stats.RangedDef + newRangedDef;
+                        var newStrBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().StrengthBonus;
+                        Stats.StrengthBonus = Stats.StrengthBonus + newStrBonus;
+                        var newRangedBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().RangedBonus;
+                        Stats.RangedBonus = Stats.RangedBonus + newRangedBonus;
+                        var newMagicBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().MagicBonus;
+                        Stats.MagicBonus = Stats.MagicBonus + newMagicBonus;
+                        var newPrayerBonus = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().PrayerBonus;
+                        Stats.PrayerBonus = Stats.PrayerBonus + newPrayerBonus;
+                        var HasSpecial = Equipment.Where(e => e.EquipmentId == item).FirstOrDefault().SpecialAttack;
+                        if (HasSpecial)
+                        {
+                            Stats.SpecialAttack = true;
+                        }
+
                     }
 
                 }
+                catch (Exception ex)
+                {
+                    ErrorMessage = ex.Message;
 
+                }
             }
-            catch (Exception ex)
-            {
-                ErrorMessage = ex.Message;
-                
-            }
-            
             return Page();
         }
         public async Task<IActionResult> OnPostFilter()
